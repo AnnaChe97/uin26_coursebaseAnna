b6e8197e8d107da995f9fece1669c8c1e33f1777
AnnaChe97 <anna_nguyen97@hotmail.com>
2026-03-23 12:26:36 +0100

    .

--- eksamensoving/pokedex-lett/src/components/PokemonCard.jsx
+++ eksamensoving/pokedex-lett/src/components/PokemonCard.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom"
 import PokemonLink from "./PokemonLink"
 export default function PokemonCard({url, classN, pokemonDef}){
     const [pokemon, setPokemon] = useState()
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState (null)
     const getPokemon = async() =>{
         // BUG: Missing try-catch block
         const response = await fetch(url)
@@ -15,6 +17,29 @@ export default function PokemonCard({url, classN, pokemonDef}){
         pokemonDef ? setPokemon(pokemonDef) : getPokemon()
     },[pokemonDef])
     
+    useEffect(()=> {
+    const getPokemon = async () => {    
+            try{
+                setLoading(true)
+                setError (null)
+
+                const response = await fetch (url)
+                const data = await response.json()
+                if (data.Response == "True") {
+                    setPokemon(data.Search)
+                }
+                else {
+                    setError ("Kunne ikke finne Pokemon")}
+                } catch (err) {
+                    setError ("Noe gikk galt med API-kallet")
+                } finally {
+                    setLoading(false)
+                }
+            }
+        }
+    )
+    
+
     return (
         <article className={`pokemon-card type-${classN}`}>
             <PokemonLink pokemon={pokemon}/>
